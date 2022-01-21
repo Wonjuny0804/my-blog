215f17222ea0561393e55904dd058b850ef8bba0
Wonjun Jang <wonwonjun@gmail.com>
2022-01-21 22:06:02 +0900

    Work on threejs examples

--- pages/lab/experiment/threejs/index.tsx
+++ pages/lab/experiment/threejs/index.tsx
@@ -2,18 +2,29 @@ import React, { FC, useState, useRef } from "react";
 import * as THREE from "three";
 import { Canvas, useFrame } from "@react-three/fiber";
 
+function useTurntable() {
+	const ref = React.useRef<THREE.Mesh>(null!);
+	useFrame(() => {
+		ref.current.rotation.y += 0.01;
+	});
+
+	return ref;
+}
+
 const Box = (props: JSX.IntrinsicElements["mesh"]) => {
-	const mesh = useRef<THREE.Mesh>(null!);
+	// const mesh = useRef<THREE.Mesh>(null!);
 
 	const [hovered, setHover] = useState<boolean>(false);
 	const [active, setActive] = useState<boolean>(false);
 
-	useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+	const ref = useTurntable();
+
+	// useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
 
 	return (
 		<mesh
 			{...props}
-			ref={mesh}
+			ref={ref}
 			scale={active ? 1.5 : 1}
 			onClick={(event) => setActive(!active)}
 			onPointerOver={(event) => setHover(true)}
@@ -31,9 +42,15 @@ const Sphere = (props: JSX.IntrinsicElements["mesh"]) => {
 	// useFrame((state, delta) => (mesh.current?.rotation.y += 0.01));
 
 	return (
-		<mesh {...props} ref={mesh} scale={2}>
-			<sphereGeometry args={[7, 12, 8]} />
-			<meshStandardMaterial color={"blue"} />
+		<mesh {...props} ref={mesh} scale={2} visible>
+			<sphereGeometry args={[7, 12, 8]} attach="geometry" />
+			<meshStandardMaterial
+				attach="material" // How the element should attach itself to its parent
+				color="#7222D3" // The color of the material
+				transparent // Defines whether this material is transparent. This has an effect on rendering as transparent objects need special treatment and are rendered after non-transparent objects. When set to true, the extent to which the material is transparent is controlled by setting it's .opacity property.
+				roughness={0.1} // The roughness of the material - Defaults to 1
+				metalness={0.1}
+			/>
 		</mesh>
 	);
 };
@@ -48,7 +65,7 @@ const PracticeOne = () => {
 				<pointLight position={[10, 10, 10]} />
 				<Box position={[-1.2, 0, 0]} />
 				<Box position={[1.2, 0, 0]} />
-				<Sphere position={[1.5, 1, 1]} />
+				<Sphere />
 			</Canvas>
 		</main>
 	);
